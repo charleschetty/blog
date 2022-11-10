da943bc24c19c660eae0b56d19c36fc13a3a8741
charleschetty <cfunction@foxmail.com>
2022-11-10 21:48:25 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:charleschetty/blog.git gh-pages
+git push -f git@github.com:charleschetty/blog.git master:gh-pages
 
 cd ..
 cd ..

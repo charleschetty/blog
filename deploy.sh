e1388d801d8ea9deb2399c710cf008d6f991173f
charleschetty <cfunction@foxmail.com>
2022-11-10 21:44:31 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+yarn run build
+cd .vitepress/dist
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:charleschetty/blog.git master:gh-pages
+
+cd ..
+cd ..
+
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:charleschetty/blog.git master
